57d1a6b4503d19473cb990699df09561709b28d0
carlynorama <carlynorama@gmail.com>
2017-06-23 10:49:03 -0700

    Initial Commit

---
+++ natcode_0101_ghostkiller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  natcode_0101_ghostkiller
+//
+//  Created by Carlyn Maw on 6/23/17.
+//  Copyright © 2017 carlynorama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

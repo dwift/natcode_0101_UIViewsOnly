579dc3c544428a5df55afbc5c5a4cb0ac5b205a3
carlynorama <carlynorama@gmail.com>
2017-06-24 12:30:19 -0700

    behaviors attached and working. TODO: Refactor

--- natcode_0101_ghostkiller/DwiftController.swift
+++ natcode_0101_ghostkiller/DwiftController.swift
@@ -16,21 +16,50 @@ class DwiftController: UIViewController, UICollisionBehaviorDelegate {
     
     var gravity: UIGravityBehavior!
     var collision: UICollisionBehavior!
+    var push: UIPushBehavior!
     
+    //var physics: UIDynamicItemBehavior!
+    
+    private lazy var physics: UIDynamicItemBehavior = {
+        let behavior = UIDynamicItemBehavior()
+        behavior.elasticity = 1
+        behavior.allowsRotation = true
+        behavior.friction = 0
+        behavior.resistance = 0
+        return behavior
+    }()
+    
+    private var balls: [BallView] {
+        return dwiftView.subviews.flatMap { $0 as? BallView }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         gravity = UIGravityBehavior(items: dwiftView.subviews)
         animator.addBehavior(gravity)
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        push = UIPushBehavior(items: dwiftView.subviews, mode: .instantaneous)
+        push.angle = -1*CGFloat.pi/2.8
+        push.magnitude = 0.7
+        animator.addBehavior(push)
+        
+        for ball in balls {
+            physics.addItem(ball)
+        }
+        animator.addBehavior(physics)
         
         collision = UICollisionBehavior(items: dwiftView.subviews)
         collision.collisionDelegate = self
         // add a boundary that has the same frame as the barrier
-        collision.addBoundary(withIdentifier: "boundry" as NSCopying, for: UIBezierPath(rect: self.dwiftView.frame)) //reference problem
         collision.translatesReferenceBoundsIntoBoundary = true
         animator.addBehavior(collision)
         
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        animator.removeAllBehaviors()
     }
 
     override func didReceiveMemoryWarning() {
